b253e44a34c919a2a2a511fde81ad4b9bf8c0bda
Alej0oo <alejofocco09@gmail.com>
2023-11-16 18:14:05 -0300

    Si no tiene camion no se muestra paquete

--- js/chofer.js
+++ js/chofer.js
@@ -1,7 +1,50 @@
 const URL = "http://localhost:8002/api/";
 
-function verPaquete(ID_Chofer) {
+function llamarFunciones(ID_Chofer){
     var ID_Chofer = document.getElementById("ID_Chofer").value;
+    document.getElementById("footer").style.position = "relative";
+
+    verCamion(ID_Chofer, function (exito) {
+        if (exito) {
+            console.log("Camion encontrado");
+            verPaquete(ID_Chofer);
+        } else {
+            console.log("ID_Chofer: " + ID_Chofer);
+        }
+        /* plataforma(ID_Chofer); */
+    });
+}
+
+
+function verCamion(ID_Chofer, callback) {
+    jQuery.ajax({
+        url: "http://localhost:8002/api/camion",
+        type: "GET",
+        data: {
+            "ID_Chofer": ID_Chofer
+        },
+        success: function (data) {
+            var tablaResultados = document.getElementById("tablaResultadosCamion");
+            tablaResultados.innerHTML = '';
+            var camion = document.createElement("tr");
+            camion.innerHTML = `
+                <td data-cell="ID Chofer">${data.camion.ID_Chofer}</td>
+                <td data-cell="ID Camion">${data.camion.ID_Camion}</td>
+                <td data-cell="Marca">${data.camion.Fecha_Hora_Inicio}</td>
+                <td data-cell="Modelo">${data.camion.ID_Estado}</td>
+                `;
+            tablaResultados.appendChild(camion);
+            callback(true);
+
+        },
+        error: function () {
+            alert("Error al cargar los datos");
+            callback(false);
+        }
+    })
+}
+
+function verPaquete(ID_Chofer) {
     jQuery.ajax({
         url: "http://localhost:8002/api/paquetes",
         type: "GET",
@@ -9,7 +52,23 @@ function verPaquete(ID_Chofer) {
             "ID_Chofer": ID_Chofer
         },
         success: function (data) {
-            var tablaResultados = document.getElementById("tablaResultados");
+            var tablaResultados = document.getElementById("tablaResultadosPaquete");
+            /* Cambiar nombre de ID_Estado
+            
+            switch(paquete.ID_Estado){
+                case 1:
+                    paquete.ID_Estado = "En almacen";
+                    break;
+                case 2:
+                    paquete.ID_Estado = "En lote";
+                    break;
+                case 3:
+                    paquete.ID_Estado = "En transito";
+                    break;
+                case 4:
+                    paquete.ID_Estado = "Entregado";
+                    break;
+            } */
             tablaResultados.innerHTML = '';
             data.paquetes.forEach(element => {
                 var fila = document.createElement("tr");
@@ -23,8 +82,8 @@ function verPaquete(ID_Chofer) {
                 if(element.ID_Estado === 2){
                     var editar = document.createElement("td");
                     editar.setAttribute("data-cell", "Editar");
-                    editar.inertHTML = `
-                        <button onclick="marcarEntregado('${ID_Chofer}', '${element.ID_Paquete}')" style="color: black;">Entregado</button>
+                    editar.innerHTML = `
+                        <button class="btn" onclick="marcarEntregado('${ID_Chofer}', '${element.ID_Paquete}')" style="color: black;">Entregado</button>
                     `;
                     fila.appendChild(editar);
                 }
@@ -36,8 +95,6 @@ function verPaquete(ID_Chofer) {
     })
 }
 
-/* <td data-cell="Marcar"><button onclick="marcarEntregado('${ID_Chofer}', '${element.ID_Paquete}')" style="color: black;">Entregado</button></td>
- */
 function marcarEntregado(ID_Chofer, ID_Paquete) {
     jQuery.ajax({
         url: "http://localhost:8002/api/paquetes",
@@ -56,18 +113,21 @@ function marcarEntregado(ID_Chofer, ID_Paquete) {
     })
 }
 
-
-function verCamion(ID_Chofer) {
-    var ID_Chofer = document.getElementById("ID_Chofer").value;
+function plataforma(ID_Chofer, accion){
+    var accion = document.getElementById("accion").value;
     jQuery.ajax({
-        url: "http://localhost:8002/api/camion",
-        type: "GET",
+        url: "http://localhost:8002/api/plataforma",
+        type: "POST",
         data: {
-            "ID_Chofer": ID_Chofer
+            "ID_Chofer": ID_Chofer,
+            "accion": accion
         },
-        success: function (data) {
-            
+        success: function () {
+            alert("Accion realizada");
+            verCamion(ID_Chofer);
+        },
+        error: function () {
+            alert("Error al cargar los datos");
         }
-        
     })
 }
